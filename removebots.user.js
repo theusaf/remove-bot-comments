cbc4257ab25e394db33c971b405ade7914f390b2
theusaf <theusafyt@gmail.com>
2022-02-14 11:47:16 -0800

    Release 0.1.0
    Add some basic youtube checks

--- removebots.user.js
+++ removebots.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide Bot Comments
 // @namespace    https://theusaf.org
-// @version      0.0.1
+// @version      0.1.0
 // @description  Removes comments made by bots on websites such as YouTube.
 // @author       theusaf
 // @match        https://www.youtube.com/**
@@ -11,7 +11,77 @@
 // @grant        none
 // ==/UserScript==
 
-const commentMutationListener = new MutationObserver((mutations) => {
+const SITES = Object.freeze({
+    YOUTUBE: [
+      /^\s{2,}/,
+      /^https:\/\/[^\s]+$/,
+      (text) => {
+        // todo: only match strings that are non-language characters, but used in bot comments.
+        // this regex currently matches non-english languages
+        const nonLetter = text.match(/[^\w]/g)?.length ?? 0;
+        return nonLetter / text.length > 0.7 && text.length > 10;
+      }
+    ]
+  }),
+  site = getCurrentSite(),
+  commentMutationListener = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      for (const node of mutation.addedNodes) {
+        const text = getCommentText(node, site);
+        if (text) {
+          if (isCommentLikelyBotComment(text, site)) {
+            node.style.display = "none";
+          }
+        }
+      }
+    }
+  });
+
+commentMutationListener.observe(document.body, {
+  subtree: true,
+  childList: true
 });
+
+/**
+ * Determines whether a comment is likely spam.
+ *
+ * @param {String} text The comment's content
+ * @param {Object} site The website the comment is from
+ * @return {Boolean}
+ */
+function isCommentLikelyBotComment(text, siteChecks) {
+  for (const check of siteChecks) {
+    if (typeof check === "function") {
+      if (check(text)) {
+        return true;
+      }
+    } else {
+      // assume regex
+      if (check.test(text)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function getCommentText(node, site) {
+  switch (site) {
+    case SITES.YOUTUBE: {
+      if (node.nodeName === "YTD-COMMENT-RENDERER") {
+        return node.querySelector("#content-text").textContent;
+      }
+    }
+  }
+  return null;
+}
+
+function getCurrentSite() {
+  switch (location.hostname) {
+    case "www.youtube.com": {
+      return SITES.YOUTUBE;
+    }
+  }
+}
 
 // ytd-comment-renderer
